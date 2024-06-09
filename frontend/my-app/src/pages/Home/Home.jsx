832662992a4a82ebd7e0ef53b8ce7c6d3656125c
Progetto2324NSD <review18r@gmail.com>
2024-06-09 12:38:45 +0200

    Update Home.jsx

--- frontend/my-app/src/pages/Home/Home.jsx
+++ frontend/my-app/src/pages/Home/Home.jsx
@@ -1,14 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import Navbar from "../../components/NavBar/Navbar";
-
+import axiosInstance from "../../utils/axiosInstance";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-    return (
-        <>
-          <Navbar />
-          
-        </>
-    )
-}
+
+  const [userInfo, setUserInfo] = useState(null);
+
+  const navigate = useNavigate();
+
+  // Get User Info
+  const getUserInfo = async () => {
+    try {
+      const response = await axiosInstance.get("/get-user");
+      if (response.data && response.data.user) {
+        setUserInfo(response.data.user);
+      }
+    } catch (error) {
+      if (error.response.status === 401) {
+        localStorage.clear();
+        navigate("/login");
+      }
+    }
+  };
+
+  useEffect(() => {
+    getUserInfo();
+    return () => {};
+  }, []);
+
+  return (
+    <>
+      <Navbar
+        userInfo={userInfo}
+      />
+    </>
+  );
+};
 
 export default Home;
