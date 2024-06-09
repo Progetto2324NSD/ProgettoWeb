dd66c20e2f953765d1c24edea6b0ec9ddd1560d7
Progetto2324NSD <review18r@gmail.com>
2024-06-09 12:38:47 +0200

    Update Login.jsx

--- frontend/my-app/src/pages/Login/Login.jsx
+++ frontend/my-app/src/pages/Login/Login.jsx
@@ -1,72 +1,88 @@
 import React, { useState } from "react";
 import Navbar from "../../components/NavBar/Navbar";
-import { Link } from "react-router-dom";
 import PasswordInput from "../../components/Input/PasswordInput";
+import axiosInstance from "../../utils/axiosInstance";
 import { validateEmail } from "../../utils/helper";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState(null);
+  const navigate = useNavigate()
 
-    const handleLogin = async (e) => {
-        e.preventDefault();
+  const handleLogin = async (e) => {
+    e.preventDefault();
 
-        if (!validateEmail(email)) {
-            setError("Inserire una email valida");
-            return;
-        }
+    if (!validateEmail(email)) {
+      setError("Inserire una email valida.");
+      return;
+    }
 
-        if (!password) {
-            setError("Inserire la password");
-            return;
-        }
+    if (!password) {
+      setError("Inserire la password.");
+      return;
+    }
 
-        setError("")
+    setError('')
 
-        //logi API call
-    };
+    //Login API Call
 
-    return (
-        <>
-            <Navbar />
+    try {
+      const response = await axiosInstance.post("/login", {
+        email: email,
+        password: password,
+      });
+      
+      // Handle successful login response 
+      if(response.data && response.data.accessToken){
+        localStorage.setItem("token", response.data.accessToken)
+        navigate('/dashboard')
+      }
 
-            <div className="flex items-center justify-center mt-28">
-                <div className="w-96 border rounded bg-white px-7 py-10">
-                    <form onSubmit={handleLogin}>
-                        <h4 className="text-2xl mb-7">Login</h4>
+    } catch (error) {
+      // Handle login error
+      if (error.response && error.response.data && error.response.data.message) {
+        setError("Utente non registrato.");
+      } else {
+        setError("Si è verificato un errore improvviso. Riprovare.");
+      }
+    }
+  };
 
-                        <input 
-                            type="text" 
-                            placeholder="Inserire l'email" 
-                            className="input-box" 
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
+  return (
+    <>
+      <Navbar />
+      <div className="flex items-center justify-center mt-28">
+        <div className="w-96 border rounded bg-white px-7 py-10">
+          <form onSubmit={handleLogin}>
+            <h4 className="text-2xl mb-7">Login</h4>
 
-                        <PasswordInput 
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
+            <input 
+              type="text" 
+              placeholder="Email" 
+              className="input-box" 
+              value={email}
+              onChange={(e) => setEmail(e.target.value)} />
 
-                        {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
+            <PasswordInput 
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
 
-                        <button type="submit" className="btn-primary">
-                            Login
-                        </button>
+            {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
 
-                        <p className="text-sm text-center mt-4">
-                            Non sei ancora registrato?{" "}
-                            <Link to="/signUp" className="font-medium text-primary underline">
-                                Crea un Account
-                            </Link>
-                        </p>
-                    </form>
-                </div>
-            </div>
-        </>
-    );
+            <button type="submit" className="btn-primary">Login</button>
+
+            <p className="text-sm text-center mt-4">
+              Not registered yet? <Link to='/signUp' className="font-medium text-primary underline">Create an Account</Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Login;
