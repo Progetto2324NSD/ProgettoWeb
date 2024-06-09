25d9e637ef690d1e968e7eb569e45bae1a3ebcf3
Progetto2324NSD <review18r@gmail.com>
2024-06-09 12:38:43 +0200

    Update Navbar.jsx

--- frontend/my-app/src/components/NavBar/Navbar.jsx
+++ frontend/my-app/src/components/NavBar/Navbar.jsx
@@ -3,13 +3,15 @@ import ProfileInfo from "../Cards/ProfileInfo";
 import { useNavigate } from "react-router-dom";
 import SearchBar from "../SearchBar/SearchBar";
 
-const Navbar = () => {
+const Navbar = ({ userInfo }) => {
+    //const isToken = localStorage.getItem("token");
 
     const [searchQuery, setSearchQuery] = useState("");
-
-    const navigate = useNavigate;
+  
+    const navigate = useNavigate();
 
     const onLogout=() => {
+        localStorage.clear()
         navigate("/login");
     };
 
@@ -24,7 +26,7 @@ const Navbar = () => {
 
     return (
         <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
-            <h2 className="text-xl font-medium text-black py-2">Notes</h2>
+            <h2 className="text-xl font-medium text-black py-2">Titolo</h2>
 
            <SearchBar 
                 value={searchQuery}
@@ -36,7 +38,7 @@ const Navbar = () => {
            /> 
 
 
-            <ProfileInfo onLogout={onLogout} />
+            <ProfileInfo userInfo={userInfo} onLogout={onLogout} />
         </div>
     );
 };
