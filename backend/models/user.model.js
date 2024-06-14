7d30a77dfc1c2238def1abd849321155d4bda60f
Progetto2324NSD <review18r@gmail.com>
2024-06-14 10:03:54 +0200

    Update user.model.js

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,5 +1,6 @@
 const mongoose=require("mongoose");
 const Schema=mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 const userSchema=new Schema({
     fullName: { type: String},
@@ -8,6 +9,20 @@ const userSchema=new Schema({
     createdOn: { type: Date, default: new Date().getTime() },
 
 });
+
+userSchema.pre("save", async function(next){
+    const user = this;
+    if(!user.isModified) return next();
+    let salt = await bcrypt.genSalt(10);
+    let hash = await bcrypt.hash(user.password, salt);
+    user.password = hash;
+    next();
+})
+
+userSchema.methods.comparePassword = async function (password){
+    return bcrypt.compare(password, this.password);
+}
+
 
 module.exports=mongoose.model("User", userSchema);
 
